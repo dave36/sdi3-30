cfaaed8e07d049253036475aa33c03ed02d64d37
dave36 <davlorenzo@hotmail.com>
2017-05-03 17:52:33 +0200

    Configuracion de EJBs terminado

--- sdi3-30.EJB/ejbModule/com/sdi/infrastructure/Factories.java
+++ sdi3-30.EJB/ejbModule/com/sdi/infrastructure/Factories.java
@@ -1,5 +1,14 @@
 package com.sdi.infrastructure;
 
+import com.sdi.persistence.Persistence;
+
 public class Factories {
-	public static ServiceFactory services = new ServicesLocator();
+	
+	private static String CONFIG_FILE = "/factories.properties";
+	
+	public static ServiceFactory services = (ServiceFactory)
+			FactoriesHelper.createFactory(CONFIG_FILE, "SERVICES_FACTORY");
+	
+	public static Persistence persistence = (Persistence)
+			FactoriesHelper.createFactory(CONFIG_FILE, "PERSISTENCE_FACTORY");
 }
